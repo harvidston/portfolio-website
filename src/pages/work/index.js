4aefb09cb6d62fa86bb3d9b07abfe2573aa89e2c
harvidston <harvidston@mail.ru>
2023-07-26 23:11:54 +0300

    Modify graduation project page

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,15 +1,55 @@
 import React, {useRef} from 'react';
-import {WorkContainer, NavigationButton} from './work-styles';
-import transition from '../../components/transition';
-
+import {WorkContainer, AdvantagesContainer, VideoContainer, GridItem, GridContainer, CardsContainer, Icon, Feature,CarouselContainer, Pictures, PicturesContainer, GitHubLink,  DescriptionItemContainer, ProjectDescriptionContainer,  Role, RoleContainer, PictureContainer, Picture,  ProjectName, Services, TitleContainer,  ProjectNameContainer} from './work-styles';
+import 'video-react/dist/video-react.css'; 
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 import { useNavigate } from 'react-router-dom';
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import Art from '../../static/images/main_project/1.png';
+import Art2 from '../../static/images/main_project/2.png';
+import Art4 from '../../static/images/main_project/6.png';
+import Art5 from '../../static/images/artproject.png';
+import Art6 from '../../static/images/main_project/8.png';
+import Art7 from '../../static/images/main_project/7.png';
+import Art9 from '../../static/images/main_project/9.png';
+import Art10 from '../../static/images/main_project/10.png';
+import Art11 from '../../static/images/main_project/11.png';
+import Art12 from '../../static/images/main_project/12.png';
+import Art13 from '../../static/images/main_project/13.png';
+import {FaUserCircle, FaCommentAlt, FaLock} from 'react-icons/fa';
+import {RiUserFollowFill} from 'react-icons/ri';
+import {BiSolidLike} from 'react-icons/bi';
+import {BsFillBasket3Fill} from 'react-icons/bs';
+import {MdPalette} from 'react-icons/md';
+import {FaFaceSmileBeam} from 'react-icons/fa6';
+import { Player } from 'video-react';
+import Video from '../../static/images/video.mp4';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Desktop2 from '../../static/images/main_project/Desktop2.png';
 
 const ArtPage = () => {
 
 	const containerRef = useRef(null)
 	const navigate = useNavigate()
+
+	const responsive = {
+		superLargeDesktop: {
+			breakpoint: { max: 2500, min: 1024 },
+			items: 1
+		},
+		desktop: {
+			breakpoint: { max: 4000, min: 1024 },
+			items: 2.2
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 1
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1
+		}
+	};
 
 	const navigationHandler = (e) => {
 		e.preventDefault()
@@ -30,10 +70,152 @@ const ArtPage = () => {
   containerRef={containerRef}
 >
   <main data-scroll-container ref={containerRef}>
-		<WorkContainer>
-			work page
-		
-		<NavigationButton onClick={navigationHandler}>Home</NavigationButton>
+		<WorkContainer data-scroll-section>
+			<TitleContainer>
+		<ProjectNameContainer ><ProjectName
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Graduation Project</ProjectName></ProjectNameContainer>
+				<ProjectDescriptionContainer >
+		<DescriptionItemContainer><Services
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Role / Services</Services><RoleContainer><Role
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Design & Development</Role></RoleContainer></DescriptionItemContainer>
+		<DescriptionItemContainer><Services
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Technology stack</Services><RoleContainer><Role
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Django • Django REST framework • React JS • Redux • PostgreSQL • JWT Authentication • Styled Components • Git • Linux</Role></RoleContainer></DescriptionItemContainer>
+		<DescriptionItemContainer><Services
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>Main Goal</Services><RoleContainer><Role
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+		>This web application is designed to develop the creative potentials of people who want to demonstrate their creative achievements on the Internet and make their work available for discussion in the creative community or who want to publish their work for sale.</Role></RoleContainer></DescriptionItemContainer>
+				</ProjectDescriptionContainer>
+			</TitleContainer>	
+			<PictureContainer>
+				<Picture
+					initial={{ scale: 0 }}
+					animate={{scale: 1}}
+					transition={{
+						type: "spring",
+						stiffness: 500,
+						damping: 90,
+					}}
+					src = {Desktop2}/>
+		<GitHubLink to = "https://github.com/harvidston" target = "_blank">GitHub</GitHubLink>
+			</PictureContainer>
+			<AdvantagesContainer>
+				<GridContainer>
+					<GridItem>
+		<Icon><FaUserCircle/></Icon>
+	 <Feature>Registration and authorization in the web application with Json Web Tokens</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><FaCommentAlt/></Icon>
+    <Feature>Send comments, reviews to the work, for feedback and development of the creative community</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><RiUserFollowFill/></Icon>
+     <Feature>Subscribe to a user with works of interest</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><FaLock/></Icon>
+     <Feature>Security of user data and works</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><FaFaceSmileBeam/></Icon>
+     <Feature>User-friendly interface for downloading and viewing user works</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><BsFillBasket3Fill/></Icon>
+     <Feature>A convenient basket of goods, where the user at any time has the function of viewing the contents of the basket and can change it at his own discretion</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon><MdPalette/></Icon>
+    <Feature>A convenient catalog of works, which contains mechanisms for categorizing and searching for works by types, tags and other parameters, for easy selection of works of interest, pagination</Feature>
+					</GridItem>
+					<GridItem>
+		<Icon>< BiSolidLike/></Icon>
+    <Feature>Send likes to the work, for feedback and development of the creative community</Feature>
+					</GridItem>
+				</GridContainer>
+			</AdvantagesContainer>
+			<PicturesContainer>
+				<CarouselContainer>
+								<CardsContainer >
+				<Carousel
+					swipeable={true}
+					swipeScrollTolerance = {10}
+					draggable={true}
+					responsive={responsive}
+					keyBoardControl={true}
+					infiniteLoop = {true}
+					thumbWidth = {10}
+				>
+					<Pictures src={Art7}/>
+					<Pictures src={Art}/>
+					<Pictures src={Art2}/>
+					<Pictures src={Art5}/>
+					<Pictures src={Art12}/>
+					<Pictures src={Art4}/>
+					<Pictures src={Art6}/>
+					<Pictures src={Art9}/>
+					<Pictures src={Art10}/>
+					<Pictures src={Art13}/>
+				</Carousel>
+		</CardsContainer>
+			</CarouselContainer>
+			</PicturesContainer>
+			<VideoContainer>
+				<Player
+					playsInline
+					poster={Art11}
+					src={Video}
+				/>
+			</VideoContainer>
 		</WorkContainer>
   </main>
 
--- src/pages/work/work-styles.js
+++ src/pages/work/work-styles.js
@@ -1,13 +1,288 @@
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {motion} from 'framer-motion';
+
 
 export const WorkContainer = styled.div`
 	display: flex;
-	height: 120vh;
+	align-items: center;
+	flex-direction: column;
+	height: 4200px;
 	width: 100%;
 	background: red;
 	background: #060508;
-	color: white;
+	//background: white;
+	color: black;
 
+	background: #171615;
+`
+export const ProjectDescriptionContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	height: 250px;
+	width: 1300px;
+	margin-top: 60px;
+	cursor: default;
+	margin-bottom: 50px;
+`
+export const DescriptionItemContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	height: 100px;
+	width: 410px;
+	cursor: default;
+`
+export const PicturesGrid = styled.div`
+	height: 600px;
+	width: 100%;
+	display: grid;
+	justify-content: center;
+	align-items: center;
+	border-radius: 30px;
+	color: black;
+	grid-template-columns: 800px 800px 800px;
+	grid-template-rows: 400px 400px; 
+	column-gap: 10px;
+	row-gap: 30px;
+`
+export const PicturesContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 600px;
+	width: 100%;
+`
+export const Pictures = styled(motion.img)`
+	height: 1000px;
+	width: 1000px;
+	object-fit: contain;
+	margin-right: 80px;
+	//filter: drop-shadow(0 0 0.55rem #FA7F28);
+	cursor: grab;
+`
+export const Services = styled(motion.div)`
+	font-size: 15px;
+	font-weight: 300;
+	text-transform: uppercase;
+	margin-bottom: 20px;
+	color: lightgray;
+	color: #FA7F28;
+    white-space: nowrap;
+
+`
+export const RoleContainer = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: center;
+	border-top: 1px solid #FA7F28;
+	font-weight: 300;
+	text-overflow: ellipsis;
+	padding-top: 30px;
+`
+export const Role = styled(motion.div)`
+	font-size: 16px;
+	color: white;
+	font-family: 'Unbounded', cursive;
+	font-weight: 300;
+	text-align: justify;
+`
+export const ProjectNameContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 200px;
+	width: 80%;
+`
+export const ProjectName = styled(motion.text)`
+	color: white;
+	font-size: 90px;
+	text-transform: uppercase;
+	cursor: default;
+`
+export const TitleContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: end;
+	justify-content: center;
+	align-items: center;
+	height: 700px;
+	width: 100%;
+	color: white;
+	background: #171615;
+`
+export const GitHubLink = styled(Link)`
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	top: 40px;
+	left: 65px;
+	border-radius: 50%;
+	height: 180px;
+	width: 180px;
+	background: red;
+	text-decoration: none;	
+	color: white;
+	font-size: 18px;
+	cursor: pointer;
+
+	background: #698a86;
+	background-image: linear-gradient(to right top, #628585, #6a8b86, #739186, #7d9787, #899c88);
+	text-transform: uppercase;
+	font-family: 'Unbounded', cursive;
+	font-weight: 600;
+
+
+`
+
+export const AdvantagesContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 1000px;
+	width: 80%;
+	color: white;
+	font-size: 20px;
+	text-transform: uppercase;
+	font-family: 'Tektur', cursive;
+`
+export const Cards2Container = styled.div`
+	height: 100%;
+	width: 100%;
+	display: grid;
+	justify-content: center;
+	align-items: center;
+	border-radius: 30px;
+	color: black;
+	grid-template-columns: 450px 450px ;
+	grid-template-rows: 450px 450px; 
+	column-gap: 10px;
+	row-gap: 10px;
+`
+export const GridContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 80%;
+	display: grid;
+	justify-content: center;
+	align-items: center;
+	grid-template-columns: 400px 400px 400px 400px;
+	grid-template-rows: 400px 400px ;
+	column-gap: 20px;
+	row-gap: 20px;
+	margin-bottom: 20px;
+	margin-top: 30px;
+`
+export const GridItem = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	height: 400px;
+	width: 400px;
+	background-image: linear-gradient(to right top, #201f1e, #1d1c1b, #191817, #161514, #121110);
+	border-radius: 60px;
+
+
+`
+export const Icon = styled.button`
+	font-size: 40px;
+	color: orange;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 200px;
+	width: 200px;
+	border-radius: 60px;
+	margin-right: 20px;
+	border: none;
+	cursor: pointer;
+	background: #201f1e;
+	color: black;	
+	transition: 0.5s;
+	margin-bottom: 20px;
+	margin-top: 30px;
+	color: #FA7F28;
+	&:hover{
+		color: lightgray;
+		scale: 1.1;
+	}
+`
+export const Feature = styled.div`
+	width: 80%;
+	font-size: 15px;
+	font-family: 'Unbounded', cursive;
+	font-weight: 300;
+	text-transform: none;
+	text-align: justify;
+
+`
+export const CardsContainer = styled.div`
+	width: 87%;
+	color: black;
+`
+export const Card = styled(motion.div)`
+	display: flex;
+	align-items: center;
+	text-align: center;
+	justify-content: center;
+	height: 450px;
+	width: 450px;
+	border-radius: 60px;
+	color:#F5F3EF;
+	background: black;
+	background: #131213;
+	font-size: 30px;
+	font-size: 40px;
+	cursor: grab;
+	user-select: none;
+	background-image: linear-gradient(to right top, #201f1e, #1d1c1b, #191817, #161514, #121110);
+	white-space: nowrap;
+
+`
+export const CarouselContainer = styled.div`
+	display: flex;
+	margin-left: 250px;
+	align-items: center;
+	height: 800px;
+	width: 100%;
+`
+export const PictureContainer = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	align-items: flex-start;
+	height: 80vh;
+	width: 65%;
+	user-select: none;
+
+`
+export const Picture = styled(motion.img)`
+	height: 100%;
+	width: 100%;
+	object-fit: contain;
+	margin-right: 80px;
+`
+export const VideoContainer = styled.div`
+	position: relative;
+	display: flex;
+	height: 800px;
+	width: 1200px;
+	justify-content: center;
+	align-items: center;
+	user-select: none;
+`
+
+export const PictureContainerMac = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 50vh;
+	width: 80%;
+	user-select: none;
 `
 export const NavigationButton = styled.button`
 	background: red;
